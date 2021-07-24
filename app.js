8d8da644ecf386c9c8a930fa7feec3aa53b53041
tanveertkd <hazarikatanveer12@gmail.com>
2021-07-24 17:55:12 +0530

    init - app works, needs styling

---
+++ app.js
@@ -0,0 +1,28 @@
+var textInput = document.querySelector("#inputArea");
+var outputTextArea = document.querySelector("#outputArea");
+var btnTranslate = document.querySelector("#btn-translate");
+
+var URL = "https://api.funtranslations.com/translate/minion.json";
+
+function urlBuilder(input){
+    return URL + "?" + "text=" + input;
+}
+
+function errorHandler(error){
+    console.log("There's been some error over here. Could you maybe try again later?", error);
+}
+
+function clickHandler(){
+    var inputText = textInput.value;
+
+    fetch(urlBuilder(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedData = json.contents.translated;
+        outputTextArea.innerText = translatedData;
+        console.log(json);
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
